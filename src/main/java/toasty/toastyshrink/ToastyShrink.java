4006f638900a94ceef272e08b203f0b14cd75136
Joshuax8 <joshua.vijsma@gmail.com>
2023-04-25 10:38:38 +0200

    rename

--- src/main/java/toasty/toastyshrink/ToastyShrink.java
+++ src/main/java/toasty/toastyshrink/ToastyShrink.java
@@ -1,4 +1,4 @@
-package toasty.shrink;
+package toasty.toastyshrink;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
--- src/main/java/toasty/toastyshrink/mixin/ToastyShrinkMixin.java
+++ src/main/java/toasty/toastyshrink/mixin/ToastyShrinkMixin.java
@@ -1,4 +1,4 @@
-package toasty.shrink.mixin;
+package toasty.toastyshrink.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
